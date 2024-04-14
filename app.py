c1d52df90c50c98e75c2889fa96330e6f30f8c4c
bhuvanrj <83565908+BHUVAN-RJ@users.noreply.github.com>
2024-04-14 19:46:18 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,8 +2,15 @@ from streamlit_webrtc import webrtc_streamer
 import av
 from ultralytics import YOLO
 import cv2
+import os
+from twilio.rest import Client
 
 model = YOLO('best.pt')
+account_sid = os.environ['TWILIO_ACCOUNT_SID']
+auth_token = os.environ['TWILIO_AUTH_TOKEN']
+client = Client(account_sid, auth_token)
+
+token = client.tokens.create()
 
 def video_frame_callback(frame):
     img = frame.to_ndarray(format="bgr24")
@@ -20,4 +27,8 @@ def video_frame_callback(frame):
     return av.VideoFrame.from_ndarray(img, format="bgr24")
 
 
-webrtc_streamer(key="example", video_frame_callback=video_frame_callback)
+webrtc_streamer(key="example", video_frame_callback=video_frame_callback, rtc_configuration={
+        "iceServers": token.ice_servers,
+        "iceTransportPolicy": "relay",
+    },media_stream_constraints={"video": True, "audio": False},
+    async_processing=True)
