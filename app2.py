9735e75a4e8c2712e6bd28fa394938db36a19952
bhuvanrj <83565908+BHUVAN-RJ@users.noreply.github.com>
2024-04-16 18:52:31 +0530

    Update app2.py

--- app2.py
+++ app2.py
@@ -10,10 +10,32 @@ from twilio.base.exceptions import TwilioRestException
 
 model = YOLO('./best.pt')
 
+def get_ice_servers():
+    try:
+        account_sid = os.environ["TWILIO_ACCOUNT_SID"]
+        auth_token = os.environ["TWILIO_AUTH_TOKEN"]
+    except KeyError:
+        logger.warning(
+            "Twilio credentials are not set. Fallback to a free STUN server from Google."  
+        )
+        return [{"urls": ["stun:stun.l.google.com:19302"]}]
+
+    client = Client(account_sid, auth_token)
+
+    try:
+        token = client.tokens.create()
+    except TwilioRestException as e:
+        st.warning(
+            f"Error occurred while accessing Twilio API. Fallback to a free STUN server from Google. ({e}), {account_sid, auth_token}"
+        )
+        return [{"urls": ["stun:stun.l.google.com:19302"]}]
+
+    return token.ice_servers
 
 
-def video_frame_callback(frame):
+def video_frame_callback(frame: av.VideoFrame) -> av.VideoFrame:
     img = frame.to_ndarray(format="bgr24")
+    img = cv2.resize(img, (256, 256), interpolation=cv2.INTER_AREA)
 
     results = model(img)[0]
 
