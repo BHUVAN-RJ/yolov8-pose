68cb041304837ae9f00c3ec9f6710037b5a7ce84
bhuvanrj <83565908+BHUVAN-RJ@users.noreply.github.com>
2024-04-16 18:16:02 +0530

    Create app2.py

---
+++ app2.py
@@ -0,0 +1,34 @@
+from streamlit_webrtc import webrtc_streamer
+import av
+import streamlit as st
+from ultralytics import YOLO
+import cv2
+import os
+from twilio.rest import Client
+from twilio.base.exceptions import TwilioRestException
+
+
+model = YOLO('./best.pt')
+
+
+
+def video_frame_callback(frame):
+    img = frame.to_ndarray(format="bgr24")
+
+    results = model(img)[0]
+
+    for result in results:
+
+        for outer_index, keypoints_batch in enumerate(result.keypoints.xy.tolist()):
+
+            for inner_index, keypoint in enumerate(keypoints_batch):
+                cv2.circle(img, (int(keypoint[0]), int(keypoint[1])), radius=3, color=(0, 0, 255), thickness=-1)
+
+    return av.VideoFrame.from_ndarray(img, format="bgr24")
+
+
+webrtc_streamer(key="example", video_frame_callback=video_frame_callback, rtc_configuration={
+        "iceServers": get_ice_servers(),
+        "iceTransportPolicy": "relay",
+    },media_stream_constraints={"video": True, "audio": False},
+    async_processing=True)
